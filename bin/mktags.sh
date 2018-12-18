c97603d3a2bf4fd3ea9b08b15a1d8c80d5cf2b0d
wooner49 <wooner49@gmail.com>
2018-12-19 03:03:09 +0900

    trivial change

--- bin/mktags.sh
+++ bin/mktags.sh
@@ -4,6 +4,7 @@
 rm -rf tags;
 ctags -R;
 # cscope
-rm -rf cscope.files cscope.out;
+rm -rf cscope.files 
+rm -rf cscope.out
 find . -name '*.c' -o -name '*.h' -o -name '*.s' -o -name '*.S' -o -print > cscope.files;
 cscope -i cscope.files
