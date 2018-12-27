1ed6f3a3e168853f4338ebdf5f76ce3e91930a3f
wooner49 <wooner49@gmail.com>
2018-12-28 00:49:00 +0900

    exclude some files from tags file

--- bin/mktags.sh
+++ bin/mktags.sh
@@ -2,9 +2,13 @@
 
 # ctags
 rm -rf tags;
-ctags -R;
+ctags -R --exclude=*.asm --exclude=.git ./* ;
 # cscope
 rm -rf cscope.files 
 rm -rf cscope.out
 find . -name '*.c' -o -name '*.h' -o -name '*.s' -o -name '*.S' -o -print > cscope.files;
-cscope -i cscope.files
+#cscope -i cscope.files
+cscope -b
+
+
+## TODO: tags에서 asm파일 못읽도록 수정 필요
